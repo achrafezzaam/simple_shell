807b66d504102c2f76937d076e2e3328f7a11a70
achrafezzaam <achraf.ezzaam@gmail.com>
2023-05-19 17:56:13 +0100

    feat: Simple Shell 0.2
    The shell can know make use of arguments.
    Tested with the command "/bin/ls -l" and "/bin/ls -l /tmp"
    The exit error seems to be corrected with this new feature.

--- main.c
+++ main.c
@@ -5,18 +5,18 @@ int main(void)
 	ssize_t count;
 	size_t len = 0;
 	char *line = NULL;
-	char *argv[] = {"", NULL};
+	char **argv;
 
 	prompt();
 	while ((count = getline(&line, &len, stdin)) != -1)
 	{
 		line[count - 1] = '\0';
-		argv[0] = line;
+		argv = cmdarr(line);
 		if (!_strcmp(line, "exit"))
 		{
 			break;
 		}
-		exec_child(argv[0], argv);
+		exec_child(argv);
 		prompt();
 	}
 	if (count == -1)
--- shell.h
+++ shell.h
@@ -14,7 +14,10 @@ int _putchar(char c);
 size_t _strlen(const char *input);
 int _strcmp(char *s1, char *s2);
 int _getchar(char c);
-void exec_child(char *cmd, char *argv[]);
+void exec_child(char *argv[]);
 void prompt(void);
+
+int getspaces(char *str);
+char **cmdarr(char *cmdline);
 
 #endif
