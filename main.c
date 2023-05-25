512e3fcb09e8b6f65fdc0613d7ca2e1865ea63ea
achrafezzaam <achraf.ezzaam@gmail.com>
2023-05-25 12:30:46 +0100

    removing the prompt

--- main.c
+++ main.c
@@ -4,23 +4,25 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	ssize_t count;
 	size_t len = 0;
 	char *line = NULL;
-	char **argv;
+	char **input;
 
-	prompt();
+	if (argc > 2)
+		prompt();
 	while ((count = getline(&line, &len, stdin)) != -1)
 	{
 		if (line[count - 1] == '\n')
 			line[count - 1] = '\0';
-		argv = cmdarr(line);
-		exec_child(argv);
-		prompt();
+		input = cmdarr(line);
+		exec_child(input);
+		if (argc > 2)
+			prompt();
 	}
-	if (count == -1)
+	if (argc > 2 && count == -1)
 		_putchar('\n');
 	free(line);
 	return (0);
