7410371a4c629ae50025bc23915cb821caf15a38
achrafezzaam <achraf.ezzaam@gmail.com>
2023-05-25 12:59:30 +0100

    Correcting betty style

--- main.c
+++ main.c
@@ -4,26 +4,26 @@
  *
  * Return: Always 0.
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(void)
 {
 	ssize_t count;
 	size_t len = 0;
 	char *line = NULL;
 	char **input;
 
-	if (argc > 2)
-		prompt();
+	/*prompt();*/
 	while ((count = getline(&line, &len, stdin)) != -1)
 	{
 		if (line[count - 1] == '\n')
 			line[count - 1] = '\0';
 		input = cmdarr(line);
 		exec_child(input);
-		if (argc > 2)
-			prompt();
+		/*prompt();*/
 	}
-	if (argc > 2 && count == -1)
-		_putchar('\n');
+	/**
+	 *if (count == -1)
+	 *	_putchar('\n');
+	 */
 	free(line);
 	return (0);
 }
